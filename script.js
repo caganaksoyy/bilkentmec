0e6206be739807e1f2d1ac4eaa6e374ff6c23925
Çağan Aksoy <caganaksoyyy@gmail.com>
2025-08-09 05:51:39 +0300

    Almost done!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Slider ve kontrol elemanlarını seç
 const slider = document.getElementById('slider');
 const btnLeft = document.getElementById('btn-left');
@@ -83,6 +85,50 @@ btnRight.addEventListener('mouseleave', () => {
   }, 2000);
 });
 
-
 // İlk dot'u aktif yap
 updateDots();
+
+  document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Animasyonu başlatmak için show ekle
+        entry.target.classList.add('show');
+      } else {
+        // Element görünmez olunca show'u kaldır, animasyon yeniden oynar
+        entry.target.classList.remove('show');
+      }
+    });
+  }, { threshold: 0.1 });
+
+  document.querySelectorAll('.fade-in-up').forEach(el => {
+    observer.observe(el);
+
+  });
+    document.querySelectorAll('.fade-in-right').forEach(el => {
+        observer.observe(el);
+
+    });
+
+});
+
+let lastScrollY = window.scrollY;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY && currentScrollY > 50) {
+    // Aşağı kaydırıyor ve biraz scroll olmuşsa gizle
+    header.style.top = `-100px`; // header yüksekliğinden fazla bir değer ver, mesela 100px
+  } else {
+    // Yukarı kaydırıyor veya çok başta scroll
+    header.style.top = '0';
+  }
+
+  lastScrollY = currentScrollY;
+});
+
+
+
+
